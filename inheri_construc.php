e5590bdc4bc5c9b90bff4bc8cb688e9b439808c7
Md Ayub Ali <ayubh9288@gmail.com>
2024-06-12 07:01:56 +0600

    Update inheri_construc.php
    update file

--- inheri_construc.php
+++ inheri_construc.php
@@ -7,6 +7,7 @@ class Father {
 }
 class Child extends Father {
     public function __construct() {
+        // get the parent class properties and method using parent::__construct() 
         parent::__construct();
         echo "This is from Child";
     }
