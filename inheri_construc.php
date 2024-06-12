892039195e2174c09ab3e155b68e9d4e38806a28
Md Ayub Ali <ayubh9288@gmail.com>
2024-06-12 06:58:52 +0600

    Update inheri_construc.php
    added inheritence construct

--- inheri_construc.php
+++ inheri_construc.php
@@ -1,1 +1,14 @@
 <?php 
+class Father {
+    public function __construct() {
+        echo "This is from parent";
+    }
+
+}
+class Child extends Father {
+    public function __construct() {
+        parent::__construct();
+        echo "This is from Child";
+    }
+}
+$obj = new Child;
