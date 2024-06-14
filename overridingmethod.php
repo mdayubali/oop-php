8aa65cbadf2eaad64e3b89e6919195f8efac8770
mdayubali88 <ayubh9288@gmail.com>
2024-06-14 08:32:49 +0600

    method override code added with comments

--- overridingmethod.php
+++ overridingmethod.php
@@ -1,8 +1,17 @@
 <?php
+//  To method override use the same method name at the clild class
 class Animal{
-    public $animal_name;
-    public function __construct(){
-    $this->name = "";
+    function animalName(){
+        echo "The Animal Name is Cow \n";
     }
 }
-$obj = new Animal();
+class Dog extends Animal{
+    function animalName(){
+        echo "Dog is override the parent method animalName \n" .parent::animalName() . " " ;
+    }
+}
+
+$objc = new Dog();
+$objc ->animalName();
+$objp = new Animal();
+$objp->animalName();
