6672b2309128f4a4e9e6dc53f9963cd91dfccc3d
mdayubali88 <ayubh9288@gmail.com>
2024-06-14 15:47:26 +0600

    namespace files and folder added with example and comments

---
+++ namespace/Cat.php
@@ -0,0 +1,10 @@
+<?php
+namespace Mew;
+class Mycat{
+    public function __construct(){
+        echo "Cat is Mewing" ."<br>";
+    }
+    public function ownMethod(){
+        echo "This is own method" . "<br>";
+    }
+}
